52d58d3af7e91ce98d2629576e8627c315bd292b
Mailde S. Ozório <32501426+Mailde@users.noreply.github.com>
2025-11-03 08:03:09 +0100

    Create plot.py

---
+++ Initial_data/plot.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+nx = np.load('f1_initial_inputs.npy')
+ny = np.load('f1_initial_outputs.npy')
+
+print('f1_initial_inputs.npy', nx)
+print('f1_initial_outputs.npy', ny)
